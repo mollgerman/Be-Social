76e5aa69070d55317c2ae75a1e680106f96b61e7
mollgerman <germandmoll2004@gmail.com>
2023-03-22 18:27:15 -0300

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,7 +5,7 @@ export const getUser = async (req, res) => {
     try {
         const { id } = req.params;
         const user = await User.findById(id)
-        res.headers("Access-Control-Allow-Origin", "*" ).status(200).json(user) 
+        res.status(200).json(user) 
     } catch (error) {
         res.status(404).json({ msg: error.message })
     }
